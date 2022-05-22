0051641cdd577bd8d6c0ec3f36298746ecf54df6
fadi <fadi99.email@gmail.com>
2022-05-23 00:13:53 +0700

    Get categories on MenuPage

--- coffeepedia/src/components/MenuCard.jsx
+++ coffeepedia/src/components/MenuCard.jsx
@@ -1,125 +1,23 @@
-export default function MenuCard() {
+export default function MenuCard({ item }) {
   return (
-    <div class="mb-[55px] grid grid-cols-2 justify-items-stretch gap-4 p-2">
-      <div className="h-[325px] w-[187px] justify-self-center">
-        <div className="flex h-[296px] w-[187px] flex-col items-center rounded-[15px] bg-[#9FC088] p-2 shadow-lg">
-          <p className="text-2xl font-semibold text-white">45K</p>
-          <div className="flex h-48 flex-col justify-center">
-            <img
-              src="https://cdn-icons-png.flaticon.com/128/1046/1046887.png"
-              alt=""
-              className="items-center"
-            />
-          </div>
-          <p className="text-l font-semibold text-white">Chocolate Caramel</p>
-        </div>
-        <div class="focus:bg mt-[-27px] ml-[37%] flex h-[51px] w-[51px] items-center justify-center rounded-full bg-white p-3 shadow-lg hover:bg-[#557B83] focus:bg-[#689099] focus:outline-none">
+    <div className="h-[325px] w-[187px] justify-self-center">
+      <div className="flex h-[296px] w-[187px] flex-col items-center rounded-[15px] bg-[#9FC088] p-2 shadow-lg">
+        <p className="text-2xl font-semibold text-white">45K</p>
+        <div className="flex h-48 flex-col justify-center">
           <img
-            src="https://cdn-icons-png.flaticon.com/128/2169/2169842.png"
-            alt="Coffeepedia Logo"
-            className="h-[27px]"
+            src="https://cdn-icons-png.flaticon.com/128/1046/1046887.png"
+            alt=""
+            className="items-center"
           />
         </div>
+        <p className="text-l font-semibold text-white">Chocolate Caramel</p>
       </div>
-      <div className="h-[325px] w-[187px] justify-self-center">
-        <div className="flex h-[296px] w-[187px] flex-col items-center rounded-[15px] bg-[#CC704B] p-2 shadow-lg">
-          <p className="text-2xl font-semibold text-white">45K</p>
-          <div className="flex h-48 flex-col justify-center">
-            <img
-              src="https://cdn-icons-png.flaticon.com/128/1046/1046887.png"
-              alt=""
-              className="items-center"
-            />
-          </div>
-          <p className="text-l font-semibold text-white">Chocolate Caramel</p>
-        </div>
-        <div class="focus:bg mt-[-27px] ml-[37%] flex h-[51px] w-[51px] items-center justify-center rounded-full bg-white  p-3 shadow-lg hover:bg-[#557B83] focus:bg-[#689099] focus:outline-none">
-          <img
-            src="https://cdn-icons-png.flaticon.com/128/2169/2169842.png"
-            alt="Coffeepedia Logo"
-            className="h-[27px]"
-          />
-        </div>
-      </div>
-      <div className="h-[325px] w-[187px] justify-self-center">
-        <div className="flex h-[296px] w-[187px] flex-col items-center rounded-[15px] bg-[#E8C07D] p-2 shadow-lg">
-          <p className="text-2xl font-semibold text-white">45K</p>
-          <div className="flex h-48 flex-col justify-center">
-            <img
-              src="https://cdn-icons-png.flaticon.com/128/1046/1046887.png"
-              alt=""
-              className="items-center"
-            />
-          </div>
-          <p className="text-l font-semibold text-white">Chocolate Caramel</p>
-        </div>
-        <div class="focus:bg mt-[-27px] ml-[37%] flex h-[51px] w-[51px] items-center justify-center rounded-full bg-white  p-3 shadow-lg hover:bg-[#557B83] focus:bg-[#689099] focus:outline-none">
-          <img
-            src="https://cdn-icons-png.flaticon.com/128/2169/2169842.png"
-            alt="Coffeepedia Logo"
-            className="h-[27px]"
-          />
-        </div>
-      </div>
-      <div className="h-[325px] w-[187px] justify-self-center">
-        <div className="flex h-[296px] w-[187px] flex-col items-center rounded-[15px] bg-[#557B83] p-2 shadow-lg">
-          <p className="text-2xl font-semibold text-white">45K</p>
-          <div className="flex h-48 flex-col justify-center">
-            <img
-              src="https://cdn-icons-png.flaticon.com/128/1046/1046887.png"
-              alt=""
-              className="items-center"
-            />
-          </div>
-          <p className="text-l font-semibold text-white">Chocolate Caramel</p>
-        </div>
-        <div class="focus:bg mt-[-27px] ml-[37%] flex h-[51px] w-[51px] items-center justify-center rounded-full bg-white  p-3 shadow-lg hover:bg-[#557B83] focus:bg-[#689099] focus:outline-none">
-          <img
-            src="https://cdn-icons-png.flaticon.com/128/2169/2169842.png"
-            alt="Coffeepedia Logo"
-            className="h-[27px]"
-          />
-        </div>
-      </div>
-      <div className="h-[325px] w-[187px] justify-self-center">
-        <div className="flex h-[296px] w-[187px] flex-col items-center rounded-[15px] bg-[#E8C07D] p-2 shadow-lg">
-          <p className="text-2xl font-semibold text-white">45K</p>
-          <div className="flex h-48 flex-col justify-center">
-            <img
-              src="https://cdn-icons-png.flaticon.com/128/1046/1046887.png"
-              alt=""
-              className="items-center"
-            />
-          </div>
-          <p className="text-l font-semibold text-white">Chocolate Caramel</p>
-        </div>
-        <div class="mt-[-27px] ml-[37%] flex h-[51px] w-[51px] items-center justify-center rounded-full bg-white p-3  shadow-lg hover:bg-[#557B83] focus:outline-none focus:ring focus:ring-[#689099]">
-          <img
-            src="https://cdn-icons-png.flaticon.com/128/2169/2169842.png"
-            alt="Coffeepedia Logo"
-            className="h-[27px]"
-          />
-        </div>
-      </div>
-      <div className="h-[325px] w-[187px] justify-self-center ">
-        <div className="flex h-[296px] w-[187px] flex-col items-center rounded-[15px] bg-[#557B83] p-2 shadow-lg">
-          <p className="text-2xl font-semibold text-white">45K</p>
-          <div className="flex h-48 flex-col justify-center">
-            <img
-              src="https://cdn-icons-png.flaticon.com/128/1046/1046887.png"
-              alt=""
-              className="items-center"
-            />
-          </div>
-          <p className="text-l font-semibold text-white">Chocolate Caramel</p>
-        </div>
-        <button class="focus:bg mt-[-27px] ml-[37%] flex h-[51px] w-[51px] items-center justify-center rounded-full bg-white p-3 shadow-lg hover:bg-[#557B83] focus:bg-[#689099] focus:outline-none">
-          <img
-            src="https://cdn-icons-png.flaticon.com/128/2169/2169842.png"
-            alt="Coffeepedia Logo"
-            className="h-[27px]"
-          />
-        </button>
+      <div className="focus:bg mt-[-27px] ml-[37%] flex h-[51px] w-[51px] items-center justify-center rounded-full bg-white p-3 shadow-lg hover:bg-[#557B83] focus:bg-[#689099] focus:outline-none">
+        <img
+          src="https://cdn-icons-png.flaticon.com/128/2169/2169842.png"
+          alt="Coffeepedia Logo"
+          className="h-[27px]"
+        />
       </div>
     </div>
   );
--- coffeepedia/src/views/MenuPage.jsx
+++ coffeepedia/src/views/MenuPage.jsx
@@ -1,8 +1,41 @@
 import HeaderNav from "../components/HeaderNav";
 import MenuCard from "../components/MenuCard";
 import search from "../assets/search.png";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function HomePage() {
+  const categoriesUrl = "http://localhost:4003/categories";
+  const [categories, setCategories] = useState({
+    loading: false,
+    error: {},
+    data: [],
+  });
+  const [selectedCategoryId, setSelectedCategoryId] = useState(-1);
+
+  useEffect(() => {
+    (async () => {
+      setCategories({ ...categories, loading: true });
+      try {
+        const { data } = await axios.get(categoriesUrl);
+        setCategories({ loading: false, error: {}, data: data });
+      } catch (error) {
+        setCategories({
+          ...categories,
+          loading: false,
+          error: error.response.data,
+        });
+      }
+    })();
+    console.log(categories.data);
+  }, []);
+
+  if (categories.loading) {
+    return <p>Loading...</p>;
+  } else if (Object.keys(categories.error).length) {
+    return <p>Error...</p>;
+  }
+
   return (
     <>
       <div
@@ -13,18 +46,21 @@ export default function HomePage() {
         <div className="px-6">
           <p className="mb-2 text-2xl font-[700]">Menu</p>
         </div>
-        <div className="flex flex-row justify-between px-6">
-          <p className="mb-2 mr-2 text-xl font-[500]">Cold Coffee</p>
-          <p className="mb-2 mr-2 text-xl font-[500] text-gray-400">
-            Hot Coffee
-          </p>
-          <p className="mb-2 mr-2 text-xl font-[500] text-gray-400">
-            Frappuccino
-          </p>
-          <p className="mb-2 mr-2 text-xl font-[500] text-gray-400">Affogato</p>
+        <div className="flex w-screen flex-row justify-between overflow-x-auto px-6">
+          <p className="mb-2 mr-2 shrink-0 text-base font-[500]">All Item</p>
+          {categories.data.map((category) => (
+            <p
+              key={category.id}
+              className="mb-2 mr-2 shrink-0 cursor-pointer text-base font-[500] text-gray-500"
+            >
+              {category.name}
+            </p>
+          ))}
         </div>
 
-        <MenuCard />
+        <div class="mb-[55px] grid grid-cols-2 justify-items-stretch gap-4 p-2">
+          <MenuCard />
+        </div>
       </div>
       <div>
         <div
