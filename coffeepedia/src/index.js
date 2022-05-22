4ef99bebb81767eaffe450ec212b47f8c2e78fdf
fadi <fadi99.email@gmail.com>
2022-05-22 22:01:07 +0700

    Merge apollo install, uninstall redux

--- coffeepedia/src/index.js
+++ coffeepedia/src/index.js
@@ -6,27 +6,18 @@ import reportWebVitals from "./reportWebVitals";
 import "flowbite";
 import { ThemeProvider } from "@material-tailwind/react";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
-import {ApolloProvider } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
 import client from "./config/apolloClient";
-
-const client = new ApolloClient({
-  uri: "",
-  cache: new InMemoryCache(),
-});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <Provider store={store}>
-        <BrowserRouter>
-          <ThemeProvider>
-            <App />
-          </ThemeProvider>
-        </BrowserRouter>
-      </Provider>
+      <BrowserRouter>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>
 );
